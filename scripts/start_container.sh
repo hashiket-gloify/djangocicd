d9a262f5b1f07ac2282b361c170e8e9128e2ce34
hashiket-gloify <122081374+hashiket-gloify@users.noreply.github.com>
2023-10-06 18:25:09 +0530

    changes

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,25 @@
-# !/bin/bash
-set -e
+#!/bin/bash
 
-docker pull hashiket/django-app
+# Stop and remove all running Docker containers
+echo "Stopping and removing all running Docker containers..."
+docker stop $(docker ps -q)
+docker rm $(docker ps -a -q)
 
-docker run -d -p 8000:8000 hashiket/django-app
+
+
+# Remove all Docker images
+echo "Removing all Docker images..."
+docker rmi $(docker images -q)
+
+
+
+# Pull new Docker images
+echo "Pulling new Docker images..."
+docker pull  hashiket/django-app-1:latest
+
+
+# Launch a new container with the latest image
+echo "Launching a new container with the latest image..."
+docker run -d --name Django-App -p 8000:8000 
+
+echo "Docker container has been updated and launched."
